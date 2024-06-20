0cd0ecc74df559611a4f1763682ae762c6818be4
호예준 <hyjcompany30@naver.com>
2024-06-20 15:52:48 +0900

    fix: bugs

--- nms/common/src/main/java/kr/starly/libs/nms/abstraction/util/ItemTranslator.java
+++ nms/common/src/main/java/kr/starly/libs/nms/abstraction/util/ItemTranslator.java
@@ -1,10 +1,13 @@
 package kr.starly.libs.nms.abstraction.util;
 
+import com.google.gson.Gson;
 import kr.starly.libs.nms.version.NmsRevision;
 import org.bukkit.inventory.ItemStack;
-import org.json.JSONObject;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Locale;
 
 public interface ItemTranslator {
@@ -15,6 +18,7 @@ public interface ItemTranslator {
     // Locale.US
     // Locale.JAPAN
     // Locale.SIMPLIFIED_CHINESE
+    @SuppressWarnings("unchecked")
     default String translateItemName(ItemStack itemStack, Locale locale) {
         NmsRevision revision = NmsRevision.REQUIRED_REVISION;
         String resourcePath = "/assets/lang/%d_%d/%s_%s.json"
@@ -23,6 +27,9 @@ public interface ItemTranslator {
         InputStream is = ItemTranslator.class.getResourceAsStream(resourcePath);
         if (is == null) return "UNTRANSLATED";
 
-        return new JSONObject(is).getString(getTranslationKey(itemStack));
+        Gson gson = new Gson();
+        InputStreamReader reader = new InputStreamReader(is, StandardCharsets.UTF_8);
+        HashMap<String, String> languageMap = gson.fromJson(reader, HashMap.class);
+        return languageMap.get(getTranslationKey(itemStack));
     }
 }
